dcc79771064ed647f01d2a8bae4f24c746b3428b
Newman Ferrer <newmanferrer@gmail.com>
2023-04-28 21:39:34 -0400

    refactor: :recycle: add new routes
    add new routes
    
    issue - 280423 - add new routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,13 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/dashboard3', '/app/:path*']
+  matcher: ['/dashboard3']
 }
+
+/* export const config = {
+  matcher: ['/dashboard3', '/app/:path*']
+} */
+
+/* export const config = {
+  matcher: ['/((?!register|api|signin).*)']
+} */
