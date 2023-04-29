12ff1f6ad4385fd3fc2c1e4cd0c0c2831077a1ed
Newman Ferrer <newmanferrer@gmail.com>
2023-04-28 21:52:30 -0400

    refactor: :recycle: refactor import component "SignInForm"
    refactor import component "SignInForm"
    
    issue - 280423 - refactor import component "SignInForm"

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,4 +1,4 @@
-import { SignInForm } from '../components/SignInForm/SignInForm'
+import { SignInForm } from '../components'
 import styles from './page.module.scss'
 
 export default function SignInPage() {
